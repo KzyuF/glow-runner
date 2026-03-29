4eef60d4a488faecfd0923717fd2644f68f7dd8f
KzyuF <kzyuf@glowvpn.com>
2026-03-29 10:03:08 +0000

    Add network_mode host

--- src/services/payment.py
+++ src/services/payment.py
@@ -3,19 +3,19 @@
 PLANS = {
     "1month": {
         "days": 30,
-        "price_stars": 40,
+        "price_stars": 130,
         "price_rub": 200,
         "label": "1 месяц (безлимит)",
     },
     "3months": {
         "days": 90,
-        "price_stars": 400,
+        "price_stars": 350,
         "price_rub": 530,
         "label": "3 месяца (безлимит)",
     },
     "6months": {
         "days": 180,
-        "price_stars": 700,
+        "price_stars": 670,
         "price_rub": 1000,
         "label": "6 месяцев (безлимит)",
     },
